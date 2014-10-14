c28a711c6e7d27d2965ca6a7f6d587398538dadb
depmely <phamquocvinh99@gmail.com>
2014-10-14 08:57:11 +0700

    site admin viet nam

--- admin/config.php
+++ admin/config.php
@@ -1,31 +1,31 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://local.congty/admin/');
-define('HTTP_CATALOG', 'http://local.congty/');
+define('HTTP_SERVER', 'http://local.adminOC/admin/');
+define('HTTP_CATALOG', 'http://local.adminOC/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://local.congty/admin/');
-define('HTTPS_CATALOG', 'http://local.congty/');
+define('HTTPS_SERVER', 'http://local.adminOC/admin/');
+define('HTTPS_CATALOG', 'http://local.adminOC/');
 
 // DIR
-define('DIR_APPLICATION', 'D:\xampp\htdocs\congty/admin/');
-define('DIR_SYSTEM', 'D:\xampp\htdocs\congty/system/');
-define('DIR_DATABASE', 'D:\xampp\htdocs\congty/system/database/');
-define('DIR_LANGUAGE', 'D:\xampp\htdocs\congty/admin/language/');
-define('DIR_TEMPLATE', 'D:\xampp\htdocs\congty/admin/view/template/');
-define('DIR_CONFIG', 'D:\xampp\htdocs\congty/system/config/');
-define('DIR_IMAGE', 'D:\xampp\htdocs\congty/image/');
-define('DIR_CACHE', 'D:\xampp\htdocs\congty/system/cache/');
-define('DIR_DOWNLOAD', 'D:\xampp\htdocs\congty/download/');
-define('DIR_FILESOURE', 'D:\xampp\htdocs\congty/filesoure/');
-define('DIR_LOGS', 'D:\xampp\htdocs\congty/system/logs/');
-define('DIR_CATALOG', 'D:\xampp\htdocs\congty/catalog/');
+define('DIR_APPLICATION', 'D:\xampp\htdocs\adminOC/admin/');
+define('DIR_SYSTEM', 'D:\xampp\htdocs\adminOC/system/');
+define('DIR_DATABASE', 'D:\xampp\htdocs\adminOC/system/database/');
+define('DIR_LANGUAGE', 'D:\xampp\htdocs\adminOC/admin/language/');
+define('DIR_TEMPLATE', 'D:\xampp\htdocs\adminOC/admin/view/template/');
+define('DIR_CONFIG', 'D:\xampp\htdocs\adminOC/system/config/');
+define('DIR_IMAGE', 'D:\xampp\htdocs\adminOC/image/');
+define('DIR_CACHE', 'D:\xampp\htdocs\adminOC/system/cache/');
+define('DIR_DOWNLOAD', 'D:\xampp\htdocs\adminOC/download/');
+define('DIR_FILESOURE', 'D:\xampp\htdocs\adminOC/filesoure/');
+define('DIR_LOGS', 'D:\xampp\htdocs\adminOC/system/logs/');
+define('DIR_CATALOG', 'D:\xampp\htdocs\adminOC/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'congty');
+define('DB_DATABASE', 'adminOC');
 define('DB_PREFIX', 'oc_');
 ?>
